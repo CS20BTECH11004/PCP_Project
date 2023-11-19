8c456cedb7da69de72769249920c97316f7e4f1c
thuptenDukpa <thuptendukpa10@gmail.com>
2023-11-19 13:57:43 +0530

    Sequential delta stepping

--- delta_stepping.cpp
+++ delta_stepping.cpp
@@ -7,14 +7,37 @@
 
 using namespace std;
 
-int delta_stepping(int n, int s, vector<vector<int>> l, float delta) {
-    vector<set<pair<int, int>>> heavy, light;
-    vector<set<int>> B;
-    vector<int> d;
+void relax(int v, int x, vector<int> &d, vector<set<int>> &B, float delta) {
+    if (x < d[v]) {
+        if (d[v] != INT_MAX)
+            B[floor(d[v]/delta)].erase(v);
+        B[floor(x / delta)].insert(v);
+        d[v] = x;
+    }
+    cout << "Tentative distances after relaxing " << v << endl;
+    cout << "[";
+    for (int i: d)
+        cout << i << " ";
+    cout << "]" << endl;
+}
+
+bool isEmpty(vector<set<int>> &B) {
+    for (set<int> s: B) {
+        if (!s.empty())
+            return false;
+    }
+    return true;
+}
+
+vector<int> delta_stepping(int n, int s, vector<vector<int>> l, float delta) {
+    vector<set<pair<int, int>>> heavy(n), light(n);
+    vector<set<int>> B(n);
+    vector<int> d(n, INT_MAX);
+    d[0] = 0;
 
     for (int v = 0; v < n; v++) {
         for (int w = 0; w < n; w++) {
-            if (w == v)
+            if (w == v || l[v][w] == 0)
                 continue;
             
             pair<int, int> vw = {v, w};
@@ -22,27 +45,56 @@ int delta_stepping(int n, int s, vector<vector<int>> l, float delta) {
                 heavy[v].insert(vw);
             else
                 light[v].insert(vw);
-
-            d[v] = INT_MAX;
         }
     }
+
+    // Print elements of the 'heavy' vector
+    std::cout << "Heavy:" << std::endl;
+    for (const auto& s : heavy) {
+        for (const auto& element : s) {
+            std::cout << "(" << element.first << ", " << element.second << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Print elements of the 'light' vector
+    std::cout << "Light:" << std::endl;
+    for (const auto& s : light) {
+        for (const auto& element : s) {
+            std::cout << "(" << element.first << ", " << element.second << ") ";
+        }
+        std::cout << std::endl;
+    }
     
-    relax(s, 0, d, B, delta);
+    //relax(s, -1, d, B, delta);
+    B[0].insert(0);
     int i = 0;
     
-    while (!B.empty()) {
+    while (!isEmpty(B)){
         set<int> S;
         set<pair<int, int>> Req;
+
+        // Print B
+        for (const auto& s : B) {
+            for (int element : s) {
+                cout << element << " ";
+            }
+            cout << endl;
+        }
 
         while (!B[i].empty()) {
             for (int v: B[i]) {
                 for(pair<int, int> vw: light[v]) {
                     Req.insert(pair<int, int> {vw.second, d[v] + l[v][vw.second]});
                 }
+            
+                // for (pair<int, int> element : Req) {
+                //     cout << "(" << element.first << ", " << element.second << ") ";
+                // } cout << endl;
                 S.insert(v);
-                B[i] = set<int> {};
             }
-
+            B[i] = set<int> {};
+            
             for (pair<int, int> vx: Req) {
                 relax(vx.first, vx.second, d, B, delta);
             }
@@ -57,6 +109,7 @@ int delta_stepping(int n, int s, vector<vector<int>> l, float delta) {
         for (pair<int, int> vx: Req) {
             relax(vx.first, vx.second, d, B, delta);
         }
+
         i++;
     }
 
@@ -65,12 +118,37 @@ int delta_stepping(int n, int s, vector<vector<int>> l, float delta) {
         sh[v] = d[v];
     }
 
+    return sh;
 }
 
-void relax(int v, int x, vector<int> &d, vector<set<int>> &B, float delta) {
-    if (x < d[v]) {
-        B[floor(d[v]/delta)].erase(v);
-        B[floor(x / delta)].insert(v);
-        d[v] = x;
-    }
+
+void printSolution(vector<int> &d){
+    cout << "Vertex \t\t\t Distance from Source" << endl;
+    for (int i = 0; i < d.size(); i++)
+        cout << i << " \t\t\t\t" << d[i] << endl;
+}
+
+int main() {
+    // vector<vector<int>> graph = {{0, 3, 0, 5, 3, 0, 3},
+    //                              {3, 0, 3, 0, 0, 0, 0},
+    //                              {0, 3, 0, 1, 0, 0, 0},
+    //                              {5, 0, 1, 0, 0, 0, 0},
+    //                              {3, 0, 0, 0, 0, 5, 0},
+    //                              {0, 0, 0, 0, 5, 0, 0},
+    //                              {3, 0, 0, 0, 0, 0, 0}};
+
+    vector<vector<int>> graph = {{0, 4, 0, 0, 8, 0, 0, 0, 0},
+                                 {4, 0, 8, 0, 11, 0, 0, 0, 0},
+                                 {0, 8, 0, 7, 0, 0, 4, 0, 2},
+                                 {0, 0, 7, 0, 0, 0, 14, 9, 0},
+                                 {8, 11, 0, 0, 0, 1, 0, 0, 7},
+                                 {0, 0, 0, 0, 1, 0, 2, 0, 6},
+                                 {0, 0, 4, 14, 0, 2, 0, 10, 0},
+                                 {0, 0, 0, 9, 0, 0, 10, 0, 0},
+                                 {0, 0, 2, 0, 7, 6, 0, 0, 0}};
+
+    float delta = 3.0;
+    vector<int> sh = delta_stepping(9, 0, graph, delta);
+
+    printSolution(sh);
 }
